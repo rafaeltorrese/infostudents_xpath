e2a61ebc053476ce265d0aff7f2ccd41aeabff15
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-12-16 19:09:34 -0600

    paths to get info

--- info_banner.py
+++ info_banner.py
@@ -2,9 +2,9 @@ import requests
 import lxml.html as html
 
 URL_HOME = ''
-XPATH_NAMES = ''
-XPATH_ID = ''
-XPATH_EMAIL = ''
+XPATH_NAMES = '//table[3]/tbody/tr/td[2]/span/text()'
+XPATH_ID = '//table[3]/tbody/tr/td[3]/span/text()'
+XPATH_EMAIL = '//table[3]/tbody/tr/td[8]/span/a/@href'
 
 
 if __name__ == '__main__':
